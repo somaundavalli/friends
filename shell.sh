4a23de7976cfa6705a802609c5883826835b1676
root <root@jaya3c.mylabserver.com>
2019-03-30 16:07:33 -0400

    uggu

--- shell.sh
+++ shell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # example of using arguments to a script
-echo "My first name is $1"
-echo "My surname is $2"
+echo "My first name is $one"
+echo "My surname is $two"
 echo "Total number of arguments is $#"
